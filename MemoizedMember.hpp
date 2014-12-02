2b10bc7db56fa2d67ecda99bf3b99615c27dc730
John Szwast <jszwast@yahoo.com>
2014-12-01 20:56:51 -0800

    Got it compiling with the proper type_traits and declval. Next: add tests. Soon: add reset method.

--- MemoizedMember.hpp
+++ MemoizedMember.hpp
@@ -7,82 +7,79 @@
 
 
 #include <utility>
+#include <type_traits>
 
 
-namespace szwast
+template<typename Key, class Class, Key (Class::*evaluate)() const>
+class MemoizedMember
 {
-	template<typename Key, class Class>
-	class MemoizedMember
+public:
+	MemoizedMember(Class& instance)
+		noexcept(std::is_nothrow_default_constructible<Key>::value)
+	: m_instance(instance)
 	{
-	public:
-		MemoizedMember(Class& instance, Key (Class::*evaluate)() const)
-		: m_instance(instance)
-		, m_evaluate(evaluate)
+	}
+
+	/**
+	 * A memoized member should be affiliated with an object.  If that object is being
+	 * assigned we want to copy the memoization state, but m_instance shouldn't change.
+	 *
+	 * Basic exception guarantee: if copying the memoized value throws, then the memoization
+	 * will be cleared and recalculated on the next request.
+	 */
+	MemoizedMember& operator=(const MemoizedMember& r)
+		noexcept(std::is_nothrow_copy_assignable<Key>::value)
+	{
+		try
 		{
+			m_valid = r.m_valid;
+			m_value = r.m_value;
 		}
-
-		/**
-		 * A memoized member should be affiliated with an object.  If that object is being
-		 * assigned we want to copy the memoization state, but m_instance shouldn't change.
-		 *
-		 * Basic exception guarantee: if copying the memoized value throws, then the memoization
-		 * will be cleared and recalculated on the next request.
-		 */
-		MemoizedMember& operator=(const MemoizedMember& r) noexcept(noexcept(m_value = r.m_value))
+		catch (...)
 		{
-			try
-			{
-				m_valid = r.m_valid;
-				m_value = r.m_value;
-			}
-			catch (...)
-			{
-				m_valid = false;
-				throw;
-			}
-			return *this;
+			m_valid = false;
+			throw;
 		}
+		return *this;
+	}
 
-		MemoizedMember& operator=(const MemoizedMember&& r)
-			noexcept(noexcept(m_value = std::move(r.m_value)))
+	MemoizedMember& operator=(const MemoizedMember&& r)
+		noexcept(std::is_nothrow_move_assignable<Key>::value)
+	{
+		try
 		{
-			try
-			{
-				m_valid = r.m_valid;
-				m_value = std::move(r.m_value);
-			}
-			catch (...)
-			{
-				m_valid = false;
-				throw;
-			}
-			return *this;
+			m_valid = r.m_valid;
+			m_value = std::move(r.m_value);
 		}
-
-		/**
-		 * Key conversion operator.  This is the meat of this class.  This is how the MemoizedMember
-		 * behaves like a Key object.  If the value has not yet been calculated, then calculate it.
-		 *
-		 * @returns	The memoized value.
-		 *
-		 * Strong exception guarantee:  If the calculation of the value to be memoized fails, then
-		 * it will be reattempted next time.
-		 */
-		operator Key() const noexcept(noexcept((m_instance.*m_evaluate)()))
+		catch (...)
 		{
-			if (!m_valid)
-			{
-				m_value = (m_instance.*m_evaluate)();
-				m_valid = true;
-			}
-			return m_value;
+			m_valid = false;
+			throw;
 		}
+		return *this;
+	}
 
-	private:
-		mutable bool m_valid{false};
-		mutable Key m_value{};
-		Class const& m_instance;
-		const Key (Class::*m_evaluate)() const;
-	};
+	/**
+	 * Key conversion operator.  This is the meat of this class.  This is how the MemoizedMember
+	 * behaves like a Key object.  If the value has not yet been calculated, then calculate it.
+	 *
+	 * @returns	The memoized value.
+	 *
+	 * Strong exception guarantee:  If the calculation of the value to be memoized fails, then
+	 * it will be reattempted next time.
+	 */
+	operator Key() const noexcept(noexcept((std::declval<Class>().*evaluate)()))
+	{
+		if (!m_valid)
+		{
+			m_value = (m_instance.*evaluate)();
+			m_valid = true;
+		}
+		return m_value;
+	}
 
-}	// namespace szwast
+private:
+	mutable bool m_valid{false};
+	mutable Key m_value{};
+	Class const& m_instance;
+};
