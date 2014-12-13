0e4aaeb264bd82c4952e7874eb46c52fa808d6f3
John Szwast <jszwast@yahoo.com>
2014-12-13 14:50:38 -0800

    Added reset and started tests. Ha ha.  The diff looks big because I changed the tabs to spaces. Fixes #1.

--- MemoizedMember.hpp
+++ MemoizedMember.hpp
@@ -1,6 +1,6 @@
 /**
- * \author John Szwast
- */
+* \author John Szwast
+*/
 
 
 #pragma once
@@ -10,76 +10,95 @@
 #include <type_traits>
 
 
-template<typename Key, class Class, Key (Class::*evaluate)() const>
+template<typename Key, class Class, Key (Class::*evaluate) () const>
 class MemoizedMember
 {
-public:
-	MemoizedMember(Class& instance)
-		noexcept(std::is_nothrow_default_constructible<Key>::value)
-	: m_instance(instance)
-	{
-	}
+  public:
+    MemoizedMember (Class& instance)
+    noexcept (std::is_nothrow_default_constructible<Key>::value)
+      : m_instance (instance)
+    {
+    }
 
-	/**
-	 * A memoized member should be affiliated with an object.  If that object is being
-	 * assigned we want to copy the memoization state, but m_instance shouldn't change.
-	 *
-	 * Basic exception guarantee: if copying the memoized value throws, then the memoization
-	 * will be cleared and recalculated on the next request.
-	 */
-	MemoizedMember& operator=(const MemoizedMember& r)
-		noexcept(std::is_nothrow_copy_assignable<Key>::value)
-	{
-		try
-		{
-			m_valid = r.m_valid;
-			m_value = r.m_value;
-		}
-		catch (...)
-		{
-			m_valid = false;
-			throw;
-		}
-		return *this;
-	}
+    MemoizedMember (Class& instance, const MemoizedMember& r)
+    noexcept (std::is_nothrow_copy_constructible<Key>::value)
+      : m_instance (instance)
+      , m_value (r.m_value)
+      , m_valid (r.m_valid)
+    {
+    }
 
-	MemoizedMember& operator=(const MemoizedMember&& r)
-		noexcept(std::is_nothrow_move_assignable<Key>::value)
-	{
-		try
-		{
-			m_valid = r.m_valid;
-			m_value = std::move(r.m_value);
-		}
-		catch (...)
-		{
-			m_valid = false;
-			throw;
-		}
-		return *this;
-	}
+    MemoizedMember (Class& instance, MemoizedMember && r)
+    noexcept (std::is_nothrow_move_constructible<Key>::value)
+      : m_instance (instance)
+      , m_value (std::move (r.m_value))
+      , m_valid (r.m_valid)
+    {
+    }
 
-	/**
-	 * Key conversion operator.  This is the meat of this class.  This is how the MemoizedMember
-	 * behaves like a Key object.  If the value has not yet been calculated, then calculate it.
-	 *
-	 * @returns	The memoized value.
-	 *
-	 * Strong exception guarantee:  If the calculation of the value to be memoized fails, then
-	 * it will be reattempted next time.
-	 */
-	operator Key() const noexcept(noexcept((std::declval<Class>().*evaluate)()))
-	{
-		if (!m_valid)
-		{
-			m_value = (m_instance.*evaluate)();
-			m_valid = true;
-		}
-		return m_value;
-	}
+    ~MemoizedMember() = default;
 
-private:
-	mutable bool m_valid{false};
-	mutable Key m_value{};
-	Class const& m_instance;
+    /**
+     * A memoized member should be affiliated with an object.  If that object is being
+     * assigned we want to copy the memoization state, but m_instance shouldn't change.
+     *
+     * Basic exception guarantee: if copying the memoized value throws, then the memoization
+     * will be cleared and recalculated on the next request.
+     */
+    MemoizedMember& operator= (const MemoizedMember& r)
+    noexcept (std::is_nothrow_copy_assignable<Key>::value)
+    {
+      m_valid = false;
+      m_value = r.m_value;
+      m_valid = r.m_valid;
+      return *this;
+    }
+
+    /**
+     * A memoized member should be affiliated with an object.  If that object is being
+     * (move) assigned we want to move the memoization state, but m_instance shouldn't change.
+     *
+     * Basic exception guarantee: if moving the memoized value throws, then the memoization
+     * will be cleared and recalculated on the next request.
+     */
+    MemoizedMember& operator= (const MemoizedMember && r)
+    noexcept (std::is_nothrow_move_assignable<Key>::value)
+    {
+      m_valid = false;
+      m_value = std::move (r.m_value);
+      m_valid = r.m_valid;
+      return *this;
+    }
+
+    /**
+     * Key conversion operator.  This is the meat of this class.  This is how the MemoizedMember
+     * behaves like a Key object.  If the value has not yet been calculated, then calculate it.
+     *
+     * @returns	The memoized value.
+     *
+     * Strong exception guarantee:  If the calculation of the value to be memoized fails, then
+     * it will be reattempted next time.
+     */
+    operator Key() const noexcept (noexcept ( (std::declval<Class>().*evaluate) ()))
+    {
+      if (!m_valid)
+      {
+        m_value = (m_instance.*evaluate) ();
+        m_valid = true;
+      }
+
+      return m_value;
+    }
+
+    /**
+     * If the owning object is mutated in a way that should change the value of the memoized
+     * member, then reset() it.  After reset(), a recomputation will occur the next time the value
+     * of the member is requested.
+     */
+    void reset() noexcept { m_valid = false; }
+
+  private:
+    Class const& m_instance;
+    mutable Key m_value {};
+    mutable bool m_valid {false};
 };
--- mm_test.cpp
+++ mm_test.cpp
@@ -3,11 +3,122 @@
  */
 
 
+#include <exception>
+
 #include "gtest/gtest.h"
 
 #include "MemoizedMember.hpp"
 
 
-TEST(dummy, standin)
+/**
+ * This struct is used for testing the exception safety of MemoizedMember.  It has flags that
+ * will trigger the throwing of an exception on the next copy, move, or eval.  The flags are
+ * cleared when they trigger, so they will only cause one exception each.
+ */
+struct ValueMightThrow
 {
+  ValueMightThrow() = default;
+  ValueMightThrow(int x) : value(x) {}
+  ValueMightThrow(ValueMightThrow const&) = default;
+  ValueMightThrow(ValueMightThrow&&) = default;
+
+  ValueMightThrow& operator=(ValueMightThrow const& r)
+  {
+    might_throw(throw_next_copy);
+    value = r.value;
+    return *this;
+  }
+
+  ValueMightThrow& operator=(ValueMightThrow&& r)
+  {
+    might_throw(throw_next_move);
+    value = r.value;
+    return *this;
+  }
+
+  friend bool operator==(ValueMightThrow const& l, ValueMightThrow const& r)
+  {
+    return l.value == r.value;
+  }
+
+  static ValueMightThrow eval()
+  {
+    eval_called = true;
+    might_throw(throw_next_eval);
+    return ValueMightThrow(next_eval_value);
+  }
+
+  static void might_throw(bool& flag)
+  {
+    if (flag)
+    {
+      flag = false;
+      throw std::exception();
+    }
+  }
+
+  static void clear()
+  {
+    throw_next_copy = false;
+    throw_next_move = false;
+    throw_next_eval = false;
+    eval_called = false;
+    next_eval_value = 0;
+  }
+
+  int value;
+
+  static bool throw_next_copy;
+  static bool throw_next_move;
+  static bool throw_next_eval;
+  static bool eval_called;
+  static int next_eval_value;
+};
+
+bool ValueMightThrow::throw_next_copy = false;
+bool ValueMightThrow::throw_next_move = false;
+bool ValueMightThrow::throw_next_eval = false;
+bool ValueMightThrow::eval_called = false;
+int ValueMightThrow::next_eval_value = 0;
+
+
+struct MemoHolder
+{
+  ValueMightThrow eval() const
+  {
+    return ValueMightThrow::eval();
+  }
+
+  MemoizedMember<ValueMightThrow, MemoHolder, &MemoHolder::eval> value{*this};
+};
+
+
+TEST(GeneralMemoization, EvalNotCalledUntilEvaluated)
+{
+  ValueMightThrow::clear();
+
+  MemoHolder x;
+  ValueMightThrow::next_eval_value = 4;
+  EXPECT_FALSE(ValueMightThrow::eval_called);
+  EXPECT_EQ(ValueMightThrow(4), x.value);
+  EXPECT_TRUE(ValueMightThrow::eval_called);
+}
+
+TEST(ExceptionSafety, ThrowingOnCopy)
+{
+  MemoHolder x;
+  MemoHolder y;
+
+  // Seed starter values into x and y.
+  ValueMightThrow::next_eval_value = 4;
+  ASSERT_EQ(4, x.value);
+  ValueMightThrow::next_eval_value = 5;
+  ASSERT_EQ(5, y.value);
+
+  // A good copy
+  x = y;
+  EXPECT_EQ(y.value, x.value);
+
+  //y.value.value = 8;
+  ADD_FAILURE() << "This test is not complete.";
 }
